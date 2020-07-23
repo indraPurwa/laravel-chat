a87f46fa883e9f755cd408b709ed61b0246cd7a5
indrapwa_laptop <gensbookcities@gmail.com>
2020-07-23 23:03:32 +0700

    schedule command

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/tes', function () {
+    echo env('APP_DEBUG');
+});
 Route::get('/sendnotif', 'OrderController@sendNotifiation');
 Route::get('/accessnotif', 'OrderController@accessNotifiation');
 
